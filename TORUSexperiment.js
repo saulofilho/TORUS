66e6d4d27823c74e07c8db76ba140bdf61957520
SauloFilho <saulosilvaf@gmail.com>
2019-03-31 20:08:58 -0300

    new visual
    new css;
    new js;
    new html;
    new variable: frame;

--- TORUSexperiment.js
+++ TORUSexperiment.js
@@ -1,54 +1,88 @@
-var slider;
-var slider2;
-var slider3;
-var slider4;
-var slider5;
+var sliderRadius;
+var sliderTubeRadius;
+var sliderRotateX ;
+var sliderRotateY ;
+var sliderRotateZ;
+var sliderRate;
 
+// var isOverSlider;
 
 function setup() {
   let canvas = createCanvas(windowWidth, windowHeight, WEBGL);
   canvas.parent('myContainer');
   background(0);
 
+  // frame rate
+  sliderRate = createSlider(1, 200, 0, 1);
+  sliderRate.parent('myInputs');
+//  sliderRate.position(900, 70);
+//  sliderRate.style('width', '150px');
+  sliderRate.class('inputs');
+
+  let textRate = createP('Frame Rate');
+  textRate.parent('myInputs');
+//  textRate.position(810, 63);
+//  textRate.style("font-size", "10px");
+
   // radius
-  slider2 = createSlider(0, 1000, 0, 0.0001);
-  slider2.position(900, 100);
-  slider2.style('width', '150px');
-  let text2 = createP('radius');
-  text2.position(830, 90);
-  text2.style("font-size", "10px");
+  sliderRadius = createSlider(0, 1000, 0, 0.0001);
+  sliderRadius.parent('myInputs');
+//  sliderRadius.position(900, 100);
+//  sliderRadius.style('width', '150px');
+  sliderRadius.class('inputs');
+
+  let textRadius = createP('Radius');
+  textRadius.parent('myInputs');
+//  textRadius.position(830, 93);
+//  textRadius.style("font-size", "10px");
 
   // tube radius
-  slider = createSlider(0.01, 100, 0.01, 0.01);
-  slider.position(900, 130);
-  slider.style('width', '150px');
-  let text1 = createP('tube radius');
-  text1.position(820, 120);
-  text1.style("font-size", "10px");
+  sliderTubeRadius = createSlider(0.01, 100, 0.01, 0.01);
+  sliderTubeRadius.parent('myInputs');
+//  sliderTubeRadius.position(900, 130);
+//  sliderTubeRadius.style('width', '150px');
+  sliderTubeRadius.class('inputs');
 
-  // rotate z
-  slider3 = createSlider(0.01, 1, 0.01, 0.001);
-  slider3.position(900, 160);
-  slider3.style('width', '150px');
-  let text3 = createP('rotate z');
-  text3.position(830, 150);
-  text3.style("font-size", "10px");
+  let textTubeRadius = createP('Tube Radius');
+  textTubeRadius.parent('myInputs');
+//  textTubeRadius.position(810, 125);
+//  textTubeRadius.style("font-size", "10px");
 
   // rotate x
-  slider4 = createSlider(0.01, 1, 0.01, 0.001);
-  slider4.position(900, 190);
-  slider4.style('width', '150px');
-  let text4 = createP('rotate x');
-  text4.position(830, 180);
-  text4.style("font-size", "10px");
+  sliderRotateX = createSlider(0.01, 1, 0.01, 0.001);
+  sliderRotateX.parent('myInputs');
+//  sliderRotateX .position(900, 160);
+//  sliderRotateX .style('width', '150px');
+  sliderRotateX .class('inputs');
+
+  let textRotateX  = createP('Rotate X');
+  textRotateX.parent('myInputs');
+//  textRotateX.position(820, 155);
+//  textRotateX.style("font-size", "10px");
 
   // rotate y
-  slider5 = createSlider(0.01, 1, 0.01, 0.001);
-  slider5.position(900, 220);
-  slider5.style('width', '150px');
-  let text5 = createP('rotate y');
-  text5.position(830, 210);
-  text5.style("font-size", "10px");
+  sliderRotateY = createSlider(0.01, 1, 0.01, 0.001);
+  sliderRotateY.parent('myInputs');
+//  sliderRotateY .position(900, 190);
+//  sliderRotateY .style('width', '150px');
+  sliderRotateY .class('inputs');
+
+  let textRotateY = createP('Rotate Y');
+  textRotateY.parent('myInputs');
+//  textRotateY.position(820, 185);
+//  textRotateY.style("font-size", "10px");
+
+  // rotate z
+  sliderRotateZ = createSlider(0.01, 1, 0.01, 0.001);
+  sliderRotateZ.parent('myInputs');
+//  sliderRotateZ.position(900, 220);
+//  sliderRotateZ.style('width', '150px');
+  sliderRotateZ.class('inputs');
+
+  let textRotateZ = createP('Rotate Z');
+  textRotateZ.parent('myInputs');
+//  textRotateZ.position(820, 215);
+//  textRotateZ.style("font-size", "10px");
 }
 
 function windowResized() {
@@ -56,19 +90,20 @@ function windowResized() {
 }
 
 function draw() {
-
-  var tubeRadius = slider.value();
-  var radius = slider2.value();
-  var rotZ = slider3.value();
-  var rotX = slider4.value();
-  var rotY = slider5.value();
+  var frameR = sliderRate.value();
+  frameRate(frameR);
+  var radius = sliderRadius.value();
+  var tubeRadius = sliderTubeRadius.value();
+  var rotX = sliderRotateX .value();
+  var rotY = sliderRotateY .value();
+  var rotZ = sliderRotateZ.value();
 
   normalMaterial();
     translate(0, 0, 500);
     push();
-    rotateZ(frameCount * rotZ);
     rotateX(frameCount * rotX);
     rotateY(frameCount * rotY);
+    rotateZ(frameCount * rotZ);
     torus(radius, tubeRadius);
     pop();
 
@@ -76,3 +111,4 @@ function draw() {
       background(0);
     }
 }
+
