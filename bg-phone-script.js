6c7896764f030262b039b8d5e393751b313b6da8
SauloFilho <saulosilvaf@gmail.com>
2019-05-25 23:01:56 -0300

    mobile bg
    sat 25 may 23h01

---
+++ bg-phone-script.js
@@ -0,0 +1,97 @@
+var sliderRadius;
+var sliderTubeRadius;
+var sliderRotateX;
+var sliderRotateY;
+var sliderRotateZ;
+var sliderRate;
+
+function setup() {
+  var canvas = createCanvas(640, 960, WEBGL);
+  canvas.parent('myContainer');
+  background(0);
+
+  // frame rate
+  sliderRate = createSlider(1, 200, 0, 1);
+  sliderRate.parent('myInputs');
+  sliderRate.class('inputs');
+
+  let textRate = createP('Frame Rate');
+  textRate.parent('myInputs');
+
+  // radius
+  sliderRadius = createSlider(0, 1000, 0, 0.0001);
+  sliderRadius.parent('myInputs');
+  sliderRadius.class('inputs');
+
+  let textRadius = createP('Radius');
+  textRadius.parent('myInputs');
+
+  // tube radius
+  sliderTubeRadius = createSlider(0.01, 100, 0.01, 0.01);
+  sliderTubeRadius.parent('myInputs');
+  sliderTubeRadius.class('inputs');
+
+  let textTubeRadius = createP('Tube Radius');
+  textTubeRadius.parent('myInputs');
+
+  // rotate x
+  sliderRotateX = createSlider(0.01, 1, 0.01, 0.001);
+  sliderRotateX.parent('myInputs');
+  sliderRotateX .class('inputs');
+
+  let textRotateX  = createP('Rotate X');
+  textRotateX.parent('myInputs');
+
+  // rotate y
+  sliderRotateY = createSlider(0.01, 1, 0.01, 0.001);
+  sliderRotateY.parent('myInputs');
+  sliderRotateY .class('inputs');
+
+  let textRotateY = createP('Rotate Y');
+  textRotateY.parent('myInputs');
+
+  // rotate z
+  sliderRotateZ = createSlider(0.01, 1, 0.01, 0.001);
+  sliderRotateZ.parent('myInputs');
+  sliderRotateZ.class('inputs');
+
+  let textRotateZ = createP('Rotate Z');
+  textRotateZ.parent('myInputs');
+
+  saveButton = createButton('save bg');
+  saveButton.mousePressed(saveImg);
+  saveButton.parent('myContainer');
+}
+
+function saveImg() {
+  save('TORUS.png');
+}
+
+function draw() {
+  var frameR = sliderRate.value();
+  frameRate(frameR);
+  var radius = sliderRadius.value();
+  var tubeRadius = sliderTubeRadius.value();
+  var rotX = sliderRotateX .value();
+  var rotY = sliderRotateY .value();
+  var rotZ = sliderRotateZ.value();
+
+  normalMaterial();
+    translate(0, 0, 500);
+    push();
+    rotateX(frameCount * rotX);
+    rotateY(frameCount * rotY);
+    rotateZ(frameCount * rotZ);
+    torus(radius, tubeRadius);
+    pop();
+
+    if (mouseIsPressed) {
+      background(0);
+    }
+}
+
+$(document).ready(function() {
+  $("button").click(function() {
+    $("span").toggle();
+  })
+})
